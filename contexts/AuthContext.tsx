08285dccc9cd8e19f291fc12370bbc4ca4483fe5
Gustavo Santos <gustavo.santos@mv.com.br>
2022-03-30 22:08:54 -0300

    Authentication with JWT: Store token

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
+import { setCookie } from 'nookies'
 import Router from 'next/router'
 
 type User = {
@@ -37,6 +38,12 @@ export function AuthProvider({ children }: AuthProviderProps) {
       })
 
       const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/' // global
+      })
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken)
 
       setUser({
         email,
