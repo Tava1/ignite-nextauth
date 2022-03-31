739cc8b53131872beab961530be4014633a7a9e5
Gustavo Santos <gustavo.santos@mv.com.br>
2022-03-30 22:51:48 -0300

    Authentication with JWT: Recovery auth data

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
-import { setCookie } from 'nookies'
+import { setCookie, parseCookies } from 'nookies'
 import Router from 'next/router'
 
 type User = {
@@ -30,6 +30,22 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>({} as User);
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies();
+
+    if (token) {
+      api.get('me').then(response => {
+        const { email, permissions, roles } = response.data
+
+        setUser({
+          email,
+          permissions,
+          roles
+        })
+      })
+    }
+  }, [])
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post('sessions', {
@@ -50,6 +66,10 @@ export function AuthProvider({ children }: AuthProviderProps) {
         permissions,
         roles
       })
+
+      console.log(`Bearer ${token}`)
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
 
       Router.push('/dashboard')
     } catch (error) {
